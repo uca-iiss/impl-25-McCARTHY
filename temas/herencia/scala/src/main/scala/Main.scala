c0768bfc5361678542d00d9c2c463ecc042d6b66
Arevalo8 <javi8arevalo.com>
2025-05-18 20:24:02 +0200

    Actualizado Main

--- temas/herencia/scala/src/main/scala/Main.scala
+++ temas/herencia/scala/src/main/scala/Main.scala
@@ -1,3 +1,17 @@
-object Main extends App {
-  println("Hola Mundo")
+abstract class Persona(val nombre: String, val edad: Int) {
+  def saludar(): String
 }
+
+class Estudiante(nombre: String, edad: Int, val universidad: String)
+  extends Persona(nombre, edad) {
+
+  override def saludar(): String = {
+    s"Hola, soy $nombre, tengo $edad años y estudio en $universidad."
+  }
+}
+
+object Main extends App {
+  val estudiante = new Estudiante("Javier", 22, "UCA")
+  println(estudiante.saludar())
+}
+
