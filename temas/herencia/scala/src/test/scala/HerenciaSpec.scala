fb0200e1b100563d05f67c2612d5f0c80dc58d93
Arevalo8 <javi8arevalo.com>
2025-05-20 18:03:45 +0200

    Actualiza HerenciaSpec.scala

--- temas/herencia/scala/src/test/scala/HerenciaSpec.scala
+++ temas/herencia/scala/src/test/scala/HerenciaSpec.scala
@@ -1,11 +1,21 @@
-import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.should.Matchers
+import org.scalatest.funsuite.AnyFunSuite
 
-class HerenciaSpec extends AnyFlatSpec with Matchers {
+class HerenciaSpec extends AnyFunSuite {
 
-  "Un Estudiante" should "saludar correctamente" in {
-    val est = new Estudiante("Ana", 21, "UCA")
-    est.saludar() should be ("Hola, soy Ana, tengo 21 años y estudio en UCA.")
+  test("Estudiante debería imprimir su info académica") {
+    val estudiante = new Estudiante("Lucía", 21, "Universidad de Cádiz")
+    assert(estudiante.infoAcademica().contains("Universidad de Cádiz"))
+  }
+
+  test("Profesor debería implementar Evaluador y Academico") {
+    val profesor = new Profesor("Dr. Gómez", 45, "Informática")
+    assert(profesor.infoAcademica().contains("Informática"))
+    assert(profesor.evaluar() == "Realizando evaluación de estudiantes...")
+  }
+
+  test("Investigador debería imprimir su especialidad") {
+    val investigadora = new Investigador("Sara", 38, "Inteligencia Artificial")
+    assert(investigadora.presentarse().contains("especialista en Inteligencia Artificial"))
   }
 }
 
