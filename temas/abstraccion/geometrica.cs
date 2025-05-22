2873bc33a91de551ef4be50f5f1f707b6445cd8d
Chrisbayy <baizan986@gmail.com>
2025-05-22 17:43:57 +0200

    Ejemplo de abstraccion

---
+++ temas/abstraccion/geometrica.cs
@@ -0,0 +1,32 @@
+using System;
+
+public interface IFormaGeometrica
+{
+    double CalcularArea();
+    double CalcularPerimetro();
+}
+
+public class Circulo : IFormaGeometrica
+{
+    public double Radio { get; }
+
+    public Circulo(double radio) => Radio = radio;
+
+    public double CalcularArea() => Math.PI * Radio * Radio;
+    public double CalcularPerimetro() => 2 * Math.PI * Radio;
+}
+
+public class Rectangulo : IFormaGeometrica
+{
+    public double Base { get; }
+    public double Altura { get; }
+
+    public Rectangulo(double @base, double altura)
+    {
+        Base = @base;
+        Altura = altura;
+    }
+
+    public double CalcularArea() => Base * Altura;
+    public double CalcularPerimetro() => 2 * (Base + Altura);
+}
