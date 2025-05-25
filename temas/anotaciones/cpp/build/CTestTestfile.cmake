0f3e0d18acefda7d1fc4be4c7285b435da93f984
David Abuin <david.abuinsanchez@alum.uca.es>
2025-05-25 23:50:22 +0200

    Changed to cpp

---
+++ temas/anotaciones/cpp/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/texmoz/Github/impl-25-GOSLING/temas/anotaciones/cpp
+# Build directory: /home/texmoz/Github/impl-25-GOSLING/temas/anotaciones/cpp/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(MyComponentTest "/home/texmoz/Github/impl-25-GOSLING/temas/anotaciones/cpp/build/runTests")
+set_tests_properties(MyComponentTest PROPERTIES  _BACKTRACE_TRIPLES "/home/texmoz/Github/impl-25-GOSLING/temas/anotaciones/cpp/CMakeLists.txt;22;add_test;/home/texmoz/Github/impl-25-GOSLING/temas/anotaciones/cpp/CMakeLists.txt;0;")
